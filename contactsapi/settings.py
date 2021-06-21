1a4f25d3488007fb8c9ea8972327c6f8ec0bb72e
Nikhilkmtnk101 <nikhil.kumar.civ17@itbhu.ac.in>
2021-06-21 14:08:39 +0530

    "additional Update"

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
